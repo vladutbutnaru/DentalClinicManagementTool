4d5afcca71feb422acff89ffe115b8ecf3e6b76f
vladutbutnaru <vlad.butnaru@feel-it-services.com>
2016-10-03 16:39:49 +0300

    Created login page with error message

---
+++ app/src/main/java/ro/dcmt/data/beans/User.java
@@ -0,0 +1,98 @@
+package ro.dcmt.data.beans;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	/**
+	 * Generated version ID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+	private String email;
+	private String firstName;
+	private String lastName;
+	private boolean isMedic;
+	private String phoneNumber;
+	private int numberOfLogins;
+	private String address;
+	private String password;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public boolean isMedic() {
+		return isMedic;
+	}
+
+	public void setMedic(boolean isMedic) {
+		this.isMedic = isMedic;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public int getNumberOfLogins() {
+		return numberOfLogins;
+	}
+
+	public void setNumberOfLogins(int numberOfLogins) {
+		this.numberOfLogins = numberOfLogins;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
